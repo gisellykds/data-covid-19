36c529df975995078d2bd4bd5739cc666a735187
Giselly K D Silva <giselly@ibm.com>
2020-06-04 18:38:55 -0300

    Adicionando estrutura inicial.

---
+++ src/main/java/br/com/data/covid19/DataCovid19Application.java
@@ -0,0 +1,13 @@
+package br.com.data.covid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCovid19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCovid19Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/data/covid19/DataCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.data.covid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCovid19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
