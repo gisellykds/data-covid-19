7d54967d3c28cc6a31a4d84517448f00b6fdf85b
gisellykds <gisellykathellynsilva@gmail.com>
2020-07-21 22:56:08 -0300

    Retirando imports nao utilizados

--- src/main/java/br/com/data/covid19/integracao/estados/service/EstadosBrasilServiceClient.java
+++ src/main/java/br/com/data/covid19/integracao/estados/service/EstadosBrasilServiceClient.java
@@ -4,7 +4,6 @@ import br.com.data.covid19.integracao.estados.bean.EstadosBean;
 import br.com.data.covid19.integracao.estados.client.EstadosBrasilClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class EstadosBrasilServiceClient {
